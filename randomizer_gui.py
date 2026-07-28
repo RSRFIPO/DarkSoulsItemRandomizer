853a3f2f2ed32f095089ad063dd6e75cc9adeef2
Reinaldo Roggiero <115185573+RSRFIPO@users.noreply.github.com>
2026-07-28 05:29:50 -0400

    Bump to 0.9.0

--- randomizer_gui.py
+++ randomizer_gui.py
@@ -31,7 +31,7 @@ INI_FILE = "randomizer.ini"
 
 MAX_SEED_LENGTH = 64
 
-VERSION_NUM = "0.8.1"
+VERSION_NUM = "0.9.0"
 # only add versions compatible RNG-wise, IE when fixing GUI stuff
 COMPATIBLE_VERSIONS = [VERSION_NUM, ]
 
